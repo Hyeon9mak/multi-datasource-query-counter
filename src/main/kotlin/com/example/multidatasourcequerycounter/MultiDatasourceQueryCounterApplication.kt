2e01491fced14d07ffad64550948471c58cee217
Hyeon9mak <jinha3507@gmail.com>
2024-09-03 15:42:30 +0900

    init project

---
+++ src/main/kotlin/com/example/multidatasourcequerycounter/MultiDatasourceQueryCounterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.multidatasourcequerycounter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiDatasourceQueryCounterApplication
+
+fun main(args: Array<String>) {
+    runApplication<MultiDatasourceQueryCounterApplication>(*args)
+}

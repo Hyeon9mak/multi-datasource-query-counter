9ab3be92121246c0c2c15de40195ca1f5d809db6
Hyeon9mak <jinha3507@gmail.com>
2024-09-05 13:53:31 +0900

    feat: jdbc connection method proxy

---
+++ src/main/kotlin/com/example/multidatasourcequerycounter/learningmanagementservice/ConnectionQueryLogger.kt
@@ -0,0 +1,36 @@
+package com.example.multidatasourcequerycounter.learningmanagementservice
+
+import org.aopalliance.intercept.MethodInterceptor
+import org.aopalliance.intercept.MethodInvocation
+import org.springframework.aop.framework.ProxyFactory
+import java.sql.Connection
+
+class ConnectionQueryLogger(
+    private val queryLog: QueryLog,
+    private val connection: Connection,
+) : MethodInterceptor {
+
+    override fun invoke(invocation: MethodInvocation): Any? {
+        val result = invocation.proceed()
+
+        if (result != null && invocation.preparedStatementInvoked()) {
+            val proxyFactory = ProxyFactory(result)
+            proxyFactory.addAdvice(PreparedStatementQueryLogger(queryLog = queryLog))
+            return proxyFactory.proxy
+        }
+
+        return result
+    }
+
+    fun getProxy(): Any {
+        val proxyFactory = ProxyFactory(connection)
+        proxyFactory.addAdvice(this)
+        return proxyFactory.proxy
+    }
+
+    private fun MethodInvocation.preparedStatementInvoked(): Boolean = this.method.name == JDBC_PREPARE_STATEMENT_METHOD_NAME
+
+    companion object {
+        private const val JDBC_PREPARE_STATEMENT_METHOD_NAME = "prepareStatement"
+    }
+}

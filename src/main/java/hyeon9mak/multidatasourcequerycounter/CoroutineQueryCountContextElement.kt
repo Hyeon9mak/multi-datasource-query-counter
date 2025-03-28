327e2e16b3b515849c8459bbc610e34b2da19a94
Hyeon9mak <jinha3507@gmail.com>
2025-03-28 16:16:54 +0900

    refactor CoroutineQueryCountContextElement for used QueryCountPerRequestHolder

--- src/main/java/hyeon9mak/multidatasourcequerycounter/CoroutineQueryCountContextElement.kt
+++ src/main/java/hyeon9mak/multidatasourcequerycounter/CoroutineQueryCountContextElement.kt
@@ -1,28 +1,27 @@
 package hyeon9mak.multidatasourcequerycounter
 
 import kotlinx.coroutines.ThreadContextElement
-import org.springframework.web.context.request.RequestAttributes
-import org.springframework.web.context.request.RequestContextHolder
 import kotlin.coroutines.CoroutineContext
 
 /**
  * https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines/-thread-context-element/
  */
-class CoroutineQueryCountContextElement(
-    private val requestAttributes: RequestAttributes = RequestContextHolder.currentRequestAttributes(),
-) : ThreadContextElement<RequestAttributes> {
+class CoroutineQueryCountContextElement : ThreadContextElement<QueryCountPerRequest?> {
 
     companion object Key : CoroutineContext.Key<CoroutineQueryCountContextElement>
 
     override val key: CoroutineContext.Key<CoroutineQueryCountContextElement>
         get() = Key
 
-    override fun updateThreadContext(context: CoroutineContext): RequestAttributes {
-        RequestContextHolder.setRequestAttributes(requestAttributes)
-        return requestAttributes
+    override fun restoreThreadContext(context: CoroutineContext, oldState: QueryCountPerRequest?) {
+        if (oldState == null) {
+            QueryCountPerRequestHolder.remove()
+        } else {
+            QueryCountPerRequestHolder.set(oldState)
+        }
     }
 
-    override fun restoreThreadContext(context: CoroutineContext, oldState: RequestAttributes) {
-        RequestContextHolder.setRequestAttributes(oldState)
+    override fun updateThreadContext(context: CoroutineContext): QueryCountPerRequest? {
+        return QueryCountPerRequestHolder.get()
     }
 }
